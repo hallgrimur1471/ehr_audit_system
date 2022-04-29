14a660ae0efc3279f844279764863d4c0174a87c
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2022-04-29 23:57:29 +0000

    Implement doctor client program

---
+++ code/doctor_client/ehr.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import argparse
+import urllib.request
+from urllib.request import Request
+from urllib.error import HTTPError
+
+EHR_SERVER_BASEURL = "http://ehr_server:5000"
+
+
+def main():
+    args = parse_arguments()
+
+    if args.verbose:
+        configure_logger(logging.DEBUG)
+    else:
+        configure_logger(logging.INFO)
+
+    do_ehr_action(
+        args.PATIENT_ID,
+        args.DOCTOR_ID,
+        args.ACTION,
+        ehr_id=args.ehr_id,
+        description=args.description,
+    )
+
+
+def do_ehr_action(patient, doctor, action, ehr_id=None, description=None):
+    if action == "CREATE":
+        ehr_server_post_request(
+            doctor, f"/create_record/{patient}", description=description
+        )
+    elif action == "DELETE":
+        ehr_server_post_request(doctor, f"/delete_record/{patient}/{ehr_id}")
+    elif action == "CHANGE":
+        ehr_server_post_request(
+            doctor, f"/change_record/{patient}/{ehr_id}", description=description
+        )
+    elif action == "GET_RECORD":
+        ehr_server_get_request(f"/get_record/{patient}/{ehr_id}/{doctor}")
+    elif action == "GET_RECORDS":
+        ehr_server_get_request(f"/get_records/{patient}/{doctor}")
+
+
+def ehr_server_post_request(doctor, url_path, description=None):
+    request_data = {"requested_by": doctor}
+    if description is not None:
+        request_data["description"] = description
+    request = Request(
+        f"{EHR_SERVER_BASEURL}{url_path}",
+        data=json.dumps(request_data).encode("utf-8"),
+        headers={"Content-type": "application/json"},
+        method="POST",
+    )
+    try:
+        with urllib.request.urlopen(request) as resp:
+            response_text = resp.read().decode()
+            logging.info(response_text.rstrip())
+    except HTTPError as err:
+        logging.error(
+            f"Failed to create record, ehr_server responded with: {err.read().decode().rstrip()}"
+        )
+
+
+def ehr_server_get_request(url_path):
+    request = Request(
+        f"{EHR_SERVER_BASEURL}{url_path}",
+        method="GET",
+    )
+    try:
+        with urllib.request.urlopen(request) as resp:
+            response_text = resp.read().decode()
+            print(response_text.rstrip())
+    except HTTPError as err:
+        logging.error(
+            f"Failed to create record, ehr_server responded with: {err.read().decode().rstrip()}"
+        )
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    valid_actions = ["CREATE", "DELETE", "CHANGE", "GET_RECORD", "GET_RECORDS"]
+    parser.description = "Do EHR operations"
+    parser.add_argument("ACTION", help=f"One of {valid_actions}")
+    parser.add_argument("PATIENT_ID", help="The name of the patient the EHR belongs to")
+    parser.add_argument(
+        "DOCTOR_ID", help="The name of the doctor that's doing the operation"
+    )
+    parser.add_argument(
+        "--description",
+        help="Put EHR contents here, "
+        + "applies to the actions CREATE and CHANGE. "
+        + "Example: 'The patient visited "
+        + "with symptoms X and Y which was remediated by Z'",
+    )
+    parser.add_argument(
+        "--ehr-id",
+        help="ID of EHR, applies to the actions DELETE, CHANGE and GET_RECORD. Example: 'f764b7595b74a1d02d3af9f8a2642b8f'",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="enables printing of debug statements",
+    )
+    arguments = parser.parse_args()
+
+    # Validate arguments
+    if arguments.ACTION not in valid_actions:
+        raise ValueError(f"ACTION must be one of {valid_actions}")
+    if arguments.ACTION == "CREATE" and arguments.description is None:
+        raise ValueError(
+            f"CREATE action must include a description, see --description flag"
+        )
+    if arguments.ACTION == "CHANGE" and arguments.description is None:
+        raise ValueError(
+            f"CHANGE action must include a new description, see --description flag"
+        )
+    if (
+        arguments.ACTION in ["DELETE", "CHANGE", "GET_RECORD"]
+        and arguments.ehr_id is None
+    ):
+        raise ValueError(
+            f"{arguments.ACTION} action must include an EHR ID, see --ehr-id flag"
+        )
+
+    return arguments
+
+
+def configure_logger(log_level):
+    logging.basicConfig(
+        format="[%(asctime)s.%(msecs)03d %(levelname)s]: %(message)s",
+        datefmt="%H:%M:%S",
+        level=log_level,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- code/ehr_server/src/ehr_server/server.py
+++ code/ehr_server/src/ehr_server/server.py
@@ -24,7 +24,14 @@ def create_record(patient):
     if "description" not in request.json:
         return "Request must include description\n", 400
 
+    if "requested_by" not in request.json:
+        return (
+            "Request must include information about "
+            + "who is requesting this EHR action"
+        ), 400
+
     description = request.json["description"]
+    requested_by = request.json["requested_by"]
 
     record = ehr.create_ehr(description)
     ehr_db.add_record(
@@ -32,9 +39,9 @@ def create_record(patient):
         record,
     )
 
-    audit.log_record(patient, "unknown", "CREATE", ehr_id=record.id)
+    audit.log_record(patient, requested_by, "CREATE", ehr_id=record.id)
 
-    log.info(f"Created record '{patient}/{record.id}'")
+    log.info(f"Created record '{patient}/{record.id}', requested by '{requested_by}'")
     return f"Created record {record.id}\n"
 
 
@@ -42,9 +49,20 @@ def create_record(patient):
 def delete_record(patient, record_id):
     log.debug(f"Deleting record '{patient}/{record_id}' ...")
 
-    ehr_db.delete_record(patient, record_id)
+    if "requested_by" not in request.json:
+        return (
+            "Request must include information about "
+            + "who is requesting this EHR action"
+        ), 400
 
-    audit.log_record(patient, "unknown", "DELETE", ehr_id=record_id)
+    requested_by = request.json["requested_by"]
+
+    try:
+        ehr_db.delete_record(patient, record_id)
+    except RuntimeError:
+        return "Failed to delete record, maybe it doesn't exist?\n", 500
+
+    audit.log_record(patient, requested_by, "DELETE", ehr_id=record_id)
 
     log.info(f"Deleted record '{patient}/{record_id}")
     return "Record deleted\n"
@@ -56,35 +74,52 @@ def change_record(patient, record_id):
 
     if "description" not in request.json:
         return "Request must include description\n", 400
+
+    if "requested_by" not in request.json:
+        return (
+            "Request must include information about "
+            + "who is requesting this EHR action"
+        ), 400
+
     new_description = request.json["description"]
+    requested_by = request.json["requested_by"]
 
-    ehr_db.change_record(patient, record_id, new_description)
+    try:
+        ehr_db.change_record(patient, record_id, new_description)
+    except RuntimeError:
+        return "Failed to change record, maybe it doesn't exist?\n", 500
 
-    audit.log_record(patient, "unknown", "CHANGE", ehr_id=record_id)
+    audit.log_record(patient, requested_by, "CHANGE", ehr_id=record_id)
 
     log.info(f"Changed record '{patient}/{record_id}'")
     return "Record changed\n"
 
 
-@app.route("/get_record/<patient>/<record_id>")
-def get_record(patient, record_id):
+@app.route("/get_record/<patient>/<record_id>/<requested_by>")
+def get_record(patient, record_id, requested_by):
     log.debug(f"Getting record '{patient}/{record_id}' ...")
 
-    record = ehr_db.get_record(patient, record_id)
+    try:
+        record = ehr_db.get_record(patient, record_id)
+    except RuntimeError:
+        return "Failed to get record, maybe it doesn't exist?\n", 500
 
-    audit.log_record(patient, "unknown", "GET_RECORD", ehr_id=record_id)
+    audit.log_record(patient, requested_by, "GET_RECORD", ehr_id=record_id)
 
     log.info(f"Fetched record '{patient}/{record_id}'")
     return str(record) + "\n"
 
 
-@app.route("/get_records/<patient>")
-def get_records(patient):
+@app.route("/get_records/<patient>/<requested_by>")
+def get_records(patient, requested_by):
     log.debug(f"Getting records of '{patient}' ...")
 
-    records = ehr_db.get_records(patient)
+    try:
+        records = ehr_db.get_records(patient)
+    except RuntimeError:
+        return "Failed to get records, maybe this patient is not in our system?\n", 500
 
-    audit.log_record(patient, "unknown", "GET_RECORDS")
+    audit.log_record(patient, requested_by, "GET_RECORDS")
 
     log.info(f"Fetched records of '{patient}'")
     return str(records) + "\n"
